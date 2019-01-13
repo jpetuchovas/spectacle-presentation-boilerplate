826708b167b24878b7905a45c2fe7949bfad8825
Justinas Petuchovas <justinas.petuchovas@gmail.com>
2019-01-13 12:10:14 +0200

    Initialize GitHub pages branch

---
+++ precache-manifest.2d5647e9af9af4c7b9da062cb45c164f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb833eefe711fe99537f",
+    "url": "./static/css/main.10bb85dd.chunk.css"
+  },
+  {
+    "revision": "bb833eefe711fe99537f",
+    "url": "./static/js/main.bb833eef.chunk.js"
+  },
+  {
+    "revision": "cbd95f440386c5900b6a",
+    "url": "./static/css/1.9c76c207.chunk.css"
+  },
+  {
+    "revision": "cbd95f440386c5900b6a",
+    "url": "./static/js/1.cbd95f44.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fd7a06e265a1e659ef1c6ccc960c8fcb",
+    "url": "./index.html"
+  }
+];
